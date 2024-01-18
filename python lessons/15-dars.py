691314121b316b0993cb7955cb67f6255b09a17d
Nasrullo <156059137+nurullayev-nasrullo@users.noreply.github.com>
2024-01-18 17:01:35 +0500

    Create 15-dars.py

---
+++ python lessons/15-dars.py
@@ -0,0 +1,27 @@
+car0 = {
+        'model':'lacetti',
+        'rang':'oq',
+        'yil':2018,
+        'narh':13000,
+        'km':50000,
+        'korobka':'avtomat'
+        }
+
+car1 = {
+        'model':'nexia 3',
+        'rang':'qora',
+        'yil':2015,
+        'narh':9000,
+        'km':89000,
+        'korobka':'mexanika'
+        }
+
+car2 = {
+        'model':'gentra',
+        'rang':'qizil',
+        'yil':2019,
+        'narh':15000,
+        'km':20000,
+        'korobka':'mexanika'
+        }
+
