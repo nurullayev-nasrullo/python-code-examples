48c195e6b2e7ad60910f5cbd43e9dbebfa43cf62
Nasrullo <156059137+nurullayev-nasrullo@users.noreply.github.com>
2024-02-08 11:56:53 +0500

    update 25-dars

--- python lessons/25-dars_words_game.py
+++ python lessons/25-dars_words_game.py
@@ -1,21 +1,43 @@
 from uzwords import words
 import random as r
 
-def get_word(x=0):
-    n = r.randint(x,31994)
-    r_word = list(words[n])
-    return r_word
+def get_word():
+    word = r.choice(words)
+    while "-" in word or ' ' in word:
+        word = r.choice(words)
+    return word.upper()
 
-word = get_word()
+def display(user_letters, word):
+    display_letter = ""
+    for letter in word:
+        if letter in user_letters.upper():
+            display_letter += letter
+        else:
+            display_letter += "-"
+    return display_letter
 
-def display(user, word):
-    arr = []
-    while True:
-        user = input('Harf kiriting: ')
-        for i in word:
-            if user == i:
-                print(i)
-            else:
-                print('-')
-        return arr
+def play():
+    word = get_word()
+    word_letters = set(word)
+    user_letters = ''
+    print(f"Мен {len(word)} хонали сўз ўйладим. Топа оласизми?")
+    while len(word_letters)>0:
+        print(display(user_letters,word))
+        if len(user_letters)>0:
+            print(f"Шу вақтгача киритилган ҳарфлар: {user_letters}")
+        
+        letter = input("Ҳарф киритинг: ").upper()
+        if letter in user_letters:
+            print("Бу ҳарфни аввал киритгансиз. Бошқа ҳарф киритинг: ")
+            continue
+        elif letter in word:
+            word_letters.remove(letter)
+            print(f"{letter} ҳарфи тўғри.")
+        else:
+            print("Бундай ҳарф йўқ.")
+        user_letters += letter
+    print(f"Табриклайман! {word} сўзини {len(user_letters)} та уриниш билан топдингиз.")
 
+
+
+play()
